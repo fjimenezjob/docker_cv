599613b9dce4ad1f6deee7434e75f70e470b5d23
Fran Jimenez <fjimenezjob@gmail.com>
2024-01-05 09:51:59 +0100

    Se genera procfile para pruebas heroku. Solucionando errores de despliegue

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,5 +7,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
